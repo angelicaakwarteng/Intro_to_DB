0a6cb6bb2e05873ebb8106f6d503a1cdf3262688
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 10:13:36 +0000

    created alx bookstore

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Books (
+book_id INT PRIMARY Key,
+title VARCHAR(130),
+author_id INT,
+price DOUBLE,
+publication_date DATE,
+FOREIGN Key (author_id) REFERENCES Authors (author_id)
+);
+
+CREATE TABLE Authors (
+author_id INT PRIMARY Key,
+author_name VARCHAR(215)
+);
+
+
+CREATE TABLE Customers (
+customer_id INT PRIMARY Key,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders (
+order_id INT Primary Key,
+customer_id INT,
+order_date DATE,
+Foreign Key(customer_id) REFERENCES Customers (customer_id)
+);
+
+CREATE TABLE Order_Details (
+orderdetailid INT PRIMARY Key,
+order_id INT,
+book_id INT,
+quantity DOUBLE,
+Foreign Key (order_id) REFERENCES Orders (order_id),
+Foreign Key (book_id) REFERENCES Orders (book_id)
+);
