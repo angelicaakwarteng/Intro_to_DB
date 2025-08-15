645c87507f0a53cb2c9685c1db6064d992c75672
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 11:48:44 +0000

    created task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 order_id INT Primary KEY,
 customer_id INT,
 order_date DATE,
-Foreign Key(customer_id) REFERENCES Customers (customer_id)
+Foreign Key(customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
