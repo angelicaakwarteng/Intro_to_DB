eb7e793c0e6a3bc0657c00e38d258b021b17e9be
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 11:47:45 +0000

    created alx bookstore

--- task_2.sql
+++ task_2.sql
@@ -1,38 +1,37 @@
-
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
 author_id INT PRIMARY Key,
 author_name VARCHAR(215)
 );
 
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
 book_id INT PRIMARY kEY,
 title VARCHAR(130),
 author_id INT,
 price DOUBLE,
 publication_date DATE,
-FOREIGN Key (author_id) REFERENCES authors (author_id)
+FOREIGN Key (author_id) REFERENCES Authors (author_id)
 );
 
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
 customer_id INT PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
 order_id INT Primary KEY,
 customer_id INT,
 order_date DATE,
-Foreign Key(customer_id) REFERENCES customers (customer_id)
+Foreign Key(customer_id) REFERENCES Customers (customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
 orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
-Foreign Key (order_id) REFERENCES orders (order_id),
-Foreign Key (book_id) REFERENCES books (book_id)
+Foreign Key (order_id) REFERENCES Orders (order_id),
+Foreign Key (book_id) REFERENCES Books (book_id)
 );
