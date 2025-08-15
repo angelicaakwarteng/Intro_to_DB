cb8d15cfe20c49079fe703a69b9d30cf25f34627
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 12:16:08 +0000

    created task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,10 @@
 USE alx_book_store;
 
 SELECT * FROM Books;
+
+SELECT  
+	COLUMN_NAME, 
+    COLUMN_TYPE, 
+    TABLE_SCHEMA = 'alx_book_store',
+    TABLE_NAME = 'Books'
+FROM INFORMATION_SCHEMA.COLUMNS;
