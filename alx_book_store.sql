177f2a6b6cae85c97a9f405abf13546892ba843b
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 10:23:50 +0000

    created alx bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,10 +25,10 @@ address TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
-order_id INT Primary KEY,
+order_id INT PRIMARY KEY,
 customer_id INT,
 order_date DATE,
-FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
