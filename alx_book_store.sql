44c7a23620eedf03b7ef81491c163d775612bd09
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 10:17:20 +0000

    created alx bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,13 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Books (
+
+CREATE TABLE IF NOT EXISTS Authors (
+author_id INT PRIMARY Key,
+author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS Books (
 book_id INT PRIMARY Key,
 title VARCHAR(130),
 author_id INT,
@@ -10,31 +16,28 @@ publication_date DATE,
 FOREIGN Key (author_id) REFERENCES Authors (author_id)
 );
 
-CREATE TABLE Authors (
-author_id INT PRIMARY Key,
-author_name VARCHAR(215)
-);
 
-
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
 customer_id INT PRIMARY Key,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
 order_id INT Primary Key,
 customer_id INT,
 order_date DATE,
 Foreign Key(customer_id) REFERENCES Customers (customer_id)
 );
 
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
 orderdetailid INT PRIMARY Key,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
 Foreign Key (order_id) REFERENCES Orders (order_id),
-Foreign Key (book_id) REFERENCES Orders (book_id)
+Foreign Key (book_id) REFERENCES Books (book_id)
 );
+
+-- DROP DATABASE alx_book_store;
