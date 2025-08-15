2b6840679684c813184ce25fb112f8163f599fa7
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 10:19:04 +0000

    created alx bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,12 +3,12 @@ USE alx_book_store;
 
 
 CREATE TABLE IF NOT EXISTS Authors (
-author_id INT PRIMARY Key,
+author_id INT PRIMARY KEY,
 author_name VARCHAR(215)
 );
 
 CREATE TABLE IF NOT EXISTS Books (
-book_id INT PRIMARY Key,
+book_id INT PRIMARY KEY,
 title VARCHAR(130),
 author_id INT,
 price DOUBLE,
@@ -18,21 +18,21 @@ FOREIGN Key (author_id) REFERENCES Authors (author_id)
 
 
 CREATE TABLE IF NOT EXISTS Customers (
-customer_id INT PRIMARY Key,
+customer_id INT PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
-order_id INT Primary Key,
+order_id INT Primary KEY,
 customer_id INT,
 order_date DATE,
 Foreign Key(customer_id) REFERENCES Customers (customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
-orderdetailid INT PRIMARY Key,
+orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
