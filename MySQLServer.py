e0e25d334797feabf264e221d0d00e9beb766891
angelicaakwarteng <angelicaakwarteng24@gmail.com>
2025-08-15 11:32:46 +0000

    created alx bookstore

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,49 @@
 import pymysql
 from lavender import password
 
-#establishing connection with my alx_book_store db
-mydb = pymysql.connect(
-    host='localhost',
-    user='root',
-    passwd=password,
-    database='alx_book_store'
-)
-#save connection object
-mydb_cursor = mydb.cursor()
+# # establishing connection with my alx_book_store db
+# try:
+#     mydb = pymysql.connect(
+#         host='localhost',
+#         user='root',
+#         passwd=password,
+#         database='alx_book_store'
+#     )
+#     #save connection object
+#     mydb_cursor = mydb.cursor()
+#     print("Database 'alx_book_store' created successfully!")
+
+# except mysql.connector.Error:
+#     print("Failing to connect to the DB.")
+
+# #handling close of the DB in the script.
+# finally:
+#     mydb_cursor.close()
+#     mydb.close()
+
+
+
+
+#using mysql-connector
+import mysql.connector
+
 try:
-    print("Database 'alx_book_store' created successfully!")
-except:
-    print("Failing to connect to the DB.")
+    my_db = mysql.connector.connect(
+    host="localhost",
+    user="alx_user",
+    password="mypassword",
+    database="alx_book_store"
+    )
 
+    # save connection object
+    mydb_cursor = my_db.cursor()
+    print("Connected to database 'alx_book_store' successfully!")
 
-#handling close of the DB in the script.
-mydb_cursor.close()
-mydb.close()
+except mysql.connector.Error as err:
+    print(f"Failed to connect: {err}")
+
+finally:
+    if 'mydb_cursor' in locals() and mydb_cursor:
+        mydb_cursor.close()
+    if 'my_db' in locals() and my_db.is_connected():
+        my_db.close()
--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,17 +3,17 @@ USE alx_book_store;
 
 
 CREATE TABLE IF NOT EXISTS Authors (
-author_id INT PRIMARY KEY,
+author_id INT PRIMARY Key,
 author_name VARCHAR(215)
 );
 
 CREATE TABLE IF NOT EXISTS Books (
-book_id INT PRIMARY KEY,
+book_id INT PRIMARY kEY,
 title VARCHAR(130),
 author_id INT,
 price DOUBLE,
 publication_date DATE,
-FOREIGN KEY (author_id) REFERENCES Authors (author_id)
+FOREIGN Key (author_id) REFERENCES Authors (author_id)
 );
 
 
@@ -25,10 +25,10 @@ address TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Orders (
-order_id INT PRIMARY KEY,
+order_id INT Primary KEY,
 customer_id INT,
 order_date DATE,
-FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+Foreign Key(customer_id) REFERENCES Customers (customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
@@ -36,8 +36,15 @@ orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
-FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-FOREIGN KEY (book_id) REFERENCES Books(book_id)
+Foreign Key (order_id) REFERENCES Orders (order_id),
+Foreign Key (book_id) REFERENCES Books (book_id)
 );
 
 -- DROP DATABASE alx_book_store;
+
+-- creating new use alx_user and passing admin rights to user to access this database.
+CREATE USER 'alx_user'@'localhost'
+IDENTIFIED WITH mysql_native_password BY 'mypassword';
+
+GRANT ALL PRIVILEGES ON alx_book_store.* TO 'alx_user'@'localhost';
+FLUSH PRIVILEGES;
